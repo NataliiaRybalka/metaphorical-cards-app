1a65b31d390dcc7bb675868705218ae7a722e7e5
NataliiaRybalka <natalirybalka21@gmail.com>
2025-03-20 11:48:57 +0200

    animation for card

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -1,5 +1,5 @@
-import { PropsWithChildren } from 'react';
-import { StyleSheet, ScrollView, Image, useWindowDimensions } from 'react-native';
+import { PropsWithChildren, useRef, useEffect } from 'react';
+import { StyleSheet, ScrollView, Image, useWindowDimensions, Animated } from 'react-native';
 import FlipCard from 'react-native-flip-card';
 import { useTranslation } from 'react-i18next';
 
@@ -9,73 +9,99 @@ import { ThemedView } from '@/components/ThemedView';
 import { cardsPaths } from '@/storage/cards_paths';
 
 type Props = PropsWithChildren<{
-	answer: { description: string; fileName: string },
+	answer: { description: string; fileName: string }
 }>;
 
 export default function Answer({ answer }: Props) {
-	const { t } = useTranslation();
+    const { t } = useTranslation();
+    
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+    const scaleAnim = useRef(new Animated.Value(0.8)).current;
 
-	const screenHeight = useWindowDimensions().height;
-	const imageHeight = screenHeight < 1000 ? '80%' : '90%';
+    const screenHeight = useWindowDimensions().height;
+    const imageHeight = screenHeight < 1000 ? '80%' : '90%';
 
-	const splitedAnswer = answer.description.split("'");
-	const title = splitedAnswer[1];
-	const description = splitedAnswer[2];
+    const splitedAnswer = answer.description.split("'");
+    const title = splitedAnswer[1];
+    const description = splitedAnswer[2];
 
-	return (
-		<ScrollView>
-			<ThemedView style={{flex: 1, height: screenHeight,}}>
-				<ThemedText type='defaultSemiBold' style={styles.title}>
-					{title}
-				</ThemedText>
+    useEffect(() => {
+        Animated.sequence([
+            Animated.delay(100),
+            Animated.parallel([
+                Animated.timing(fadeAnim, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(scaleAnim, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+            ]),
+        ]).start();
+    }, []);
 
-				<FlipCard flipHorizontal={true} flipVertical={false} style={styles.flipCard}>
-					<ThemedView>
-						<Image
-							source={cardsPaths[answer.fileName]}
-							style={{width: '100%', height: imageHeight, borderRadius: 10}}
-						/>
+    return (
+        <ScrollView>
+            <ThemedText type="defaultSemiBold" style={styles.title}>
+                {title}
+            </ThemedText>
 
-						<ThemedText style={styles.disclaimer}>
-							{t('Click on the image to read the meaning')}
-						</ThemedText>
-					</ThemedView>
+            <Animated.View style={{ flex: 1, height: screenHeight, opacity: fadeAnim, transform: [{ scale: scaleAnim }] }}>
+                <FlipCard flipHorizontal flipVertical={false} style={styles.flipCard}>
+                    <ThemedView>
+                        <Image source={cardsPaths[answer.fileName]} style={[{ height: imageHeight }, styles.image]} />
+                        <ThemedText style={styles.disclaimer}>
+                            {t('Click on the image to read the meaning')}
+                        </ThemedText>
+                    </ThemedView>
 
-					<ThemedView>
-						<ThemedText style={styles.description}>
-							{description}
-						</ThemedText>
-
-						<ThemedText style={styles.disclaimer}>
-							{t('Click on the text to return to the image')}
-						</ThemedText>
-					</ThemedView>
-				</FlipCard>
-			</ThemedView>
-		</ScrollView>
-	);
+					<ThemedView style={{marginTop: 30}}>
+                        <ThemedText style={styles.description}>{description}</ThemedText>
+                        <ThemedText style={styles.disclaimer}>
+                            {t('Click on the text to return to the image')}
+                        </ThemedText>
+                    </ThemedView>
+                </FlipCard>
+            </Animated.View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-	title: {
+    title: {
 		padding: 10,
-		paddingTop: 30,
 		textAlign: 'center',
 		fontSize: 30,
-	},
-	flipCard: {
+    },
+    flipCard: {
 		padding: 10,
 		paddingTop: 0,
-	},
-	description: {
+		paddingBottom: 0,
+    },
+    description: {
 		paddingTop: 50,
-		paddingBottom: 50,
+		paddingBottom: 10,
 		fontSize: 20,
-	},
-	disclaimer: {
+    },
+    disclaimer: {
 		textAlign: 'center',
 		fontSize: 12,
-		paddingTop: 10,
 		color: 'gray',
-	},
+		paddingTop: 5,
+    },
+    image: {
+		width: '100%',
+		borderRadius: 10,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+    },
 });
